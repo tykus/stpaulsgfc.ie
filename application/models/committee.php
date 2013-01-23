92b205ee737e29cd031accac2c434eb955673987
Brian O'Sullivan <brian.osullivan@live.com>
2013-01-23 15:50:01 +0000

    Added data vaildation to the Committee model

--- application/models/committee.php
+++ application/models/committee.php
@@ -2,5 +2,20 @@
 	
 	class Committee extends Eloquent 
 	{
+		
+		// Using non-conventional database table name (compared with model name) 	
 		public static $table = 'committee_members';
+		
+		
+		// Validation Rules
+		public static $rules = array(
+			'name' => 'required|min:2',
+			'role' => 'required'
+		);
+		
+		// Data validation method
+		public static function validate($data)
+		{
+			return Validator::make($data, static::$rules);
+		}
 	}
--- application/views/committees/new.blade.php
+++ application/views/committees/new.blade.php
@@ -2,22 +2,31 @@
 
 @section('content')
 	<div class="span12">
+		
+		<!-- Display any errors -->
+		@if($errors->has())
+		<ul>
+			{{ $errors->first('name', '<li>:message</li>) }}
+			{{ $errors->first('role', '<li>:message</li>) }}			
+		</ul>
+		@endif
+		
 		{{ Form::open('committees/create', 'POST') }}
 		<p>
 			{{ Form::label('name', 'Name') }}<br />
-			{{ Form::text('name') }}
+			{{ Form::text('name', Input::old('name')) }}
 		</p>
 		<p>
 			{{ Form::label('role', 'Role') }}<br />
-			{{ Form::text('role') }}
+			{{ Form::text('role', Input::old('role')) }}
 		</p>
 		<p>
 			{{ Form::label('telephone', 'Telephone') }}<br />
-			{{ Form::text('telephone') }}
+			{{ Form::text('telephone', Input::old('telephone')) }}
 		</p>
 		<p>
 			{{ Form::label('email', 'Email') }}<br />
-			{{ Form::text('email') }}
+			{{ Form::text('email', Input::old('email')) }}
 		</p>
 		<p>
 			{{ Form::submit('Add committee member') }}
